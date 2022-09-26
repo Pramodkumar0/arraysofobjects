fd02194ffe291ee51aeaca458d500d1732f803c9
Pramodkumar0 <109613194+Pramodkumar0@users.noreply.github.com>
2022-09-26 21:44:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+ let studentRecords = [ 
+                        {name: 'John', id: 123, marks : 98 },
+                        {name: 'Baba', id: 101, marks : 23 },  
+                        {name: 'yaga', id: 200, marks : 45 },
+                        {name: 'Wick', id: 115, marks : 75 } 
+                    ] 
+                
+
+    
+    //    let name = studentRecords.name.toUpperCase();
+    //     console.log(name)
+
+// let sum = 0; 
+// for(let i = 0;i<studentRecords.length;i++){
+//     if(studentRecords[i].marks>50){
+//         sum=sum+studentRecords[i].marks
+//     }
+//     else{
+//         let add = studentRecords[i].marks+15;
+//     }
+// }
